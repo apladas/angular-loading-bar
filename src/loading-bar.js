0dc50a83dff270ab94b8193e93fb5cebcdd56b96
Kenn Jaakson <mail@kennjaakson.com>
2016-12-29 02:19:47 +0200

    Removed blurring

--- src/loading-bar.js
+++ src/loading-bar.js
@@ -167,205 +167,188 @@ angular.module('cfp.loadingBar', [])
     this.latencyThreshold = 100;
     this.startSize = 0.02;
     this.parentSelector = 'body';
-    this.loaderParentSelector = 'div#view';
-    this.blurSelector = 'div#view > div#view_content';
-    this.loaderTemplate = '<div class="page-loader"></div>';
-    this.spinnerTemplate = '<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>';
-    this.loadingBarTemplate = '<div id="loading-bar"><div class="bar"><div class="peg"></div></div></div>';
+      this.$get = ['$injector', '$document', '$timeout', '$rootScope', function ($injector, $document, $timeout, $rootScope) {
+          var $animate;
+          var $parentSelector = this.parentSelector,
+              $loaderParentSelector = this.loaderParentSelector,
+              loaderContainer = angular.element(this.loaderTemplate),
+              loadingBarContainer = angular.element(this.loadingBarTemplate),
+              loadingBar = loadingBarContainer.find('div').eq(0),
+              spinner = angular.element(this.spinnerTemplate);
 
-    this.$get = ['$injector', '$document', '$timeout', '$rootScope', function ($injector, $document, $timeout, $rootScope) {
-      var $animate;
-      var $parentSelector = this.parentSelector,
-        $blurSelector = this.blurSelector,
-        $loaderParentSelector = this.loaderParentSelector,
-        loaderContainer = angular.element(this.loaderTemplate),
-        loadingBarContainer = angular.element(this.loadingBarTemplate),
-        loadingBar = loadingBarContainer.find('div').eq(0),
-        spinner = angular.element(this.spinnerTemplate),
-        blurElement = null;
+          var incTimeout,
+              completeTimeout,
+              started = false,
+              status = 0;
 
-      var incTimeout,
-        completeTimeout,
-        started = false,
-        status = 0;
+          var includeBlurring = this.includeBlurring;
+          var autoIncrement = this.autoIncrement;
+          var includeSpinner = this.includeSpinner;
+          var includeBar = this.includeBar;
+          var startSize = this.startSize;
 
-      var includeBlurring = this.includeBlurring;
-      var autoIncrement = this.autoIncrement;
-      var includeSpinner = this.includeSpinner;
-      var includeBar = this.includeBar;
-      var startSize = this.startSize;
+          /**
+           * Inserts the loading bar element into the dom, and sets it to 2%
+           */
+          function _start() {
+              if (!$animate) {
+                  $animate = $injector.get('$animate');
+              }
 
-      /**
-       * Inserts the loading bar element into the dom, and sets it to 2%
-       */
-      function _start() {
-        if (!$animate) {
-          $animate = $injector.get('$animate');
-        }
+              $timeout.cancel(completeTimeout);
 
-        $timeout.cancel(completeTimeout);
+              // do not continually broadcast the started event
+              if (started) {
+                  return;
+              }
 
-        // do not continually broadcast the started event
-        if (started) {
-          return;
-        }
+              var document = $document[0];
+              var parent = document.querySelector ?
+                      document.querySelector($parentSelector)
+                      : $document.find($parentSelector)[0]
+                  ;
 
-        var document = $document[0];
-        var parent = document.querySelector ?
-          document.querySelector($parentSelector)
-          : $document.find($parentSelector)[0]
-        ;
+              var loaderParentElement = document.querySelector ?
+                  document.querySelector($loaderParentSelector) :
+                  $document.find($loaderParentSelector)[0];
 
-        var loaderParentElement = document.querySelector ?
-            document.querySelector($loaderParentSelector) :
-            $document.find($loaderParentSelector)[0];
+              if (! parent) {
+                  parent = document.getElementsByTagName('body')[0];
+              }
 
-        if (! parent) {
-          parent = document.getElementsByTagName('body')[0];
-        }
+              var $parent = angular.element(parent);
+              var $after = parent.lastChild && angular.element(parent.lastChild);
 
-        var $parent = angular.element(parent);
-        var $after = parent.lastChild && angular.element(parent.lastChild);
+              $rootScope.$broadcast('cfpLoadingBar:started');
+              started = true;
 
-        $rootScope.$broadcast('cfpLoadingBar:started');
-        started = true;
+              if (includeBar) {
+                  $animate.enter(loadingBarContainer, $parent, $after);
+              }
 
-        if (includeBar) {
-          $animate.enter(loadingBarContainer, $parent, $after);
-        }
+              if (includeSpinner) {
+                  $animate.enter(spinner, $parent, loadingBarContainer);
+              }
 
-        if (includeSpinner) {
-          $animate.enter(spinner, $parent, loadingBarContainer);
-        }
+              if (includeBlurring && loaderParentElement) {
+                  $animate.enter(loaderContainer, loaderParentElement);
+              }
 
-        if (includeBlurring && loaderParentElement) {
-          $animate.enter(loaderContainer, loaderParentElement);
-        }
-
-        _set(startSize);
-      }
-
-      /**
-       * Set the loading bar's width to a certain percent.
-       *
-       * @param n any value between 0 and 1
-       */
-      function _set(n) {
-        if (!started) {
-          return;
-        }
-        var pct = (n * 100) + '%';
-        loadingBar.css('width', pct);
-        status = n;
-
-        if (!blurElement || blurElement.length < 1 || n < 0.1) blurElement = $document.querySelector ?
-            $document.querySelector($blurSelector) :
-            $document.find($blurSelector);
-
-        if (includeBlurring && blurElement) {
-
-          var blurpx = 20 * (1 - n);
-          blurElement.css({
-            'filter': 'blur(' + blurpx + 'px)',
-            '-webkit-filter': 'blur(' + blurpx + 'px)',
-            '-o-filter': 'blur(' + blurpx + 'px)',
-            'opacity': 0.5 + 0.5 * n
-          });
-        }
-
-        // increment loadingbar to give the illusion that there is always
-        // progress but make sure to cancel the previous timeouts so we don't
-        // have multiple incs running at the same time.
-        if (autoIncrement) {
-          $timeout.cancel(incTimeout);
-          incTimeout = $timeout(function() {
-            _inc();
-          }, 250);
-        }
-      }
-
-      /**
-       * Increments the loading bar by a random amount
-       * but slows down as it progresses
-       */
-      function _inc() {
-        if (_status() >= 1) {
-          return;
-        }
-
-        var rnd = 0;
-
-        // TODO: do this mathmatically instead of through conditions
-
-        var stat = _status();
-        if (stat >= 0 && stat < 0.25) {
-          // Start out between 3 - 6% increments
-          rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
-        } else if (stat >= 0.25 && stat < 0.65) {
-          // increment between 0 - 3%
-          rnd = (Math.random() * 3) / 100;
-        } else if (stat >= 0.65 && stat < 0.9) {
-          // increment between 0 - 2%
-          rnd = (Math.random() * 2) / 100;
-        } else if (stat >= 0.9 && stat < 0.99) {
-          // finally, increment it .5 %
-          rnd = 0.005;
-        } else {
-          // after 99%, don't increment:
-          rnd = 0;
-        }
-
-        var pct = _status() + rnd;
-        _set(pct);
-      }
-
-      function _status() {
-        return status;
-      }
-
-      function _completeAnimation() {
-        status = 0;
-        started = false;
-      }
-
-      function _complete() {
-        if (!$animate) {
-          $animate = $injector.get('$animate');
-        }
-
-        _set(1);
-        $animate.leave(loaderContainer);
-        $timeout.cancel(completeTimeout);
-
-        // Attempt to aggregate any start/complete calls within 500ms:
-        completeTimeout = $timeout(function() {
-          var promise = $animate.leave(loadingBarContainer, _completeAnimation);
-          if (promise && promise.then) {
-            promise.then(_completeAnimation);
+              _set(startSize);
           }
-          $animate.leave(spinner);
-          $rootScope.$broadcast('cfpLoadingBar:completed');
-        }, 500);
-      }
 
-      return {
-        start            : _start,
-        set              : _set,
-        status           : _status,
-        inc              : _inc,
-        complete         : _complete,
-        autoIncrement    : this.autoIncrement,
-        includeSpinner   : this.includeSpinner,
-        latencyThreshold : this.latencyThreshold,
-        parentSelector   : this.parentSelector,
-        startSize        : this.startSize,
-        includeBlurring  : this.includeBlurring,
-        loaderTemplate   : this.loaderTemplate,
-        loaderParentSelector: this.loaderParentSelector,
-        loaderSelector   : this.loaderSelector
-      };
+          /**
+           * Set the loading bar's width to a certain percent.
+           *
+           * @param n any value between 0 and 1
+           */
+          function _set(n) {
+              if (!started) {
+                  return;
+              }
+              var pct = (n * 100) + '%';
+              loadingBar.css('width', pct);
+              status = n;
 
 
-    }];     //
+              // increment loadingbar to give the illusion that there is always
+              // progress but make sure to cancel the previous timeouts so we don't
+              // have multiple incs running at the same time.
+              if (autoIncrement) {
+                  $timeout.cancel(incTimeout);
+                  incTimeout = $timeout(function() {
+                      _inc();
+                  }, 250);
+              }
+          }
+
+          /**
+           * Increments the loading bar by a random amount
+           * but slows down as it progresses
+           */
+          function _inc() {
+              if (_status() >= 1) {
+                  return;
+              }
+
+              var rnd = 0;
+
+              // TODO: do this mathmatically instead of through conditions
+
+              var stat = _status();
+              if (stat >= 0 && stat < 0.25) {
+                  // Start out between 3 - 6% increments
+                  rnd = (Math.random() * (5 - 3 + 1) + 3) / 100;
+              } else if (stat >= 0.25 && stat < 0.65) {
+                  // increment between 0 - 3%
+                  rnd = (Math.random() * 3) / 100;
+              } else if (stat >= 0.65 && stat < 0.9) {
+                  // increment between 0 - 2%
+                  rnd = (Math.random() * 2) / 100;
+              } else if (stat >= 0.9 && stat < 0.99) {
+                  // finally, increment it .5 %
+                  rnd = 0.005;
+              } else {
+                  // after 99%, don't increment:
+                  rnd = 0;
+              }
+
+              var pct = _status() + rnd;
+              _set(pct);
+          }
+
+          function _status() {
+              return status;
+          }
+
+          function _completeAnimation() {
+              status = 0;
+              started = false;
+          }
+
+          function _complete() {
+              if (!$animate) {
+                  $animate = $injector.get('$animate');
+              }
+
+              _set(1);
+              $animate.leave(loaderContainer);
+              $timeout.cancel(completeTimeout);
+
+              // Attempt to aggregate any start/complete calls within 500ms:
+              completeTimeout = $timeout(function() {
+                  var promise = $animate.leave(loadingBarContainer, _completeAnimation);
+                  if (promise && promise.then) {
+                      promise.then(_completeAnimation);
+                  }
+                  $animate.leave(spinner);
+                  $rootScope.$broadcast('cfpLoadingBar:completed');
+              }, 500);
+          }
+
+          return {
+              start            : _start,
+              set              : _set,
+              status           : _status,
+              inc              : _inc,
+              complete         : _complete,
+              autoIncrement    : this.autoIncrement,
+              includeSpinner   : this.includeSpinner,
+              latencyThreshold : this.latencyThreshold,
+              parentSelector   : this.parentSelector,
+              startSize        : this.startSize,
+              includeBlurring  : this.includeBlurring,
+              loaderTemplate   : this.loaderTemplate,
+              loaderParentSelector: this.loaderParentSelector,
+              loaderSelector   : this.loaderSelector
+          };
+
+
+      }];     //,,,
+      this.loaderParentSelector = 'div#view';
+      this.loaderTemplate = '<div class="page-loader"></div>';
+    this.spinnerTemplate = '<div id="loading-bar-spinner"><div class="spinner-icon"></div></div>';
+
+    this.loadingBarTemplate = '<div id="loading-bar"><div class="bar"><div class="peg"></div></div></div>';
   });       // wtf javascript. srsly
 })();       //
